30128e187adae08799753a85c6ed545a6b53ad15
thesergioliveira <thesergioliveira@gmail.com>
2021-07-16 12:35:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
-      <h1>Hello world!</h1>
+      <Navbar />
+      <div class="main-container">
+        <h1>Main content</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend
+          odio sed mi ullamcorper fermentum. Fusce vestibulum tincidunt dictum.
+          Aliquam et quam neque. Pellentesque rutrum congue sem ultrices
+          convallis. Nullam facilisis iaculis augue, ac tristique dui feugiat
+          suscipit. Fusce et lorem eu metus placerat tincidunt in id magna.
+          Curabitur ut euismod dolor, quis vulputate odio. In hac habitasse
+          platea dictumst. Mauris vitae sagittis libero. Quisque scelerisque,
+          est id ultricies feugiat, nibh mauris consectetur justo, quis sodales
+          nisi mauris eu sapien. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus. Mauris eu nisi odio. Etiam
+          interdum lacus et augue euismod feugiat. Sed iaculis nunc vitae ante
+          pulvinar pharetra. Proin bibendum quis magna nec elementum. Donec
+          lacinia nisi dolor, ac convallis libero auctor vitae. Aliquam erat
+          volutpat. Aliquam fermentum augue id eros tristique, id commodo eros
+          sollicitudin. Ut pulvinar quam accumsan, dictum est eget, vehicula
+          magna. Etiam mattis metus ut risus eleifend vehicula. Aliquam eu
+          pulvinar libero. Nulla sit amet mollis diam, in cursus erat. Sed
+          dapibus tortor at ultrices tincidunt. Phasellus sed augue odio. Proin
+          lectus justo, volutpat id metus non, pellentesque varius sapien. In a
+          risus lacus. Suspendisse potenti. Etiam euismod enim vitae neque
+          lobortis porttitor. Quisque.
+        </p>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,72 @@
+import React, { Fragment, useState } from "react";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    switch (theme) {
+      case "dark":
+        setTheme("dark");
+        break;
+      case "light":
+        setTheme("light");
+        break;
+      case "solar":
+        setTheme("solar");
+        break;
+      default:
+        setTheme("gray");
+    }
+  };
+
+  return (
+    <Fragment>
+      <nav>
+        <ul>
+          <li>
+            <span></span>
+            <a>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M10.024 4h6.015l7.961 8-7.961 8h-6.015l7.961-8-7.961-8zm-10.024 16h6.015l7.961-8-7.961-8h-6.015l7.961 8-7.961 8z" />
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a></a>
+            <span>Home</span>
+          </li>
+          <li>
+            <a></a>
+            <span>About</span>
+          </li>
+          <li>
+            <a>
+              <svg></svg>
+            </a>
+            <span>Shop</span>
+          </li>
+          <li>
+            <a>
+              <svg></svg>
+            </a>
+            <span>Contact</span>
+          </li>
+          <li>
+            <a>
+              <svg></svg>
+            </a>
+            <span>Change Theme</span>
+          </li>
+        </ul>
+      </nav>
+    </Fragment>
+  );
+};
+
+export default Navbar;
